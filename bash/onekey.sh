79d7044a07cbcc2c5dd4254fee634c3339ea55ca
linbing <linbing@semptian.com>
2015-03-04 00:15:19 -0800

    1. add bash_completion

---
+++ bash/onekey.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#  Filename: onekey.sh
+#   Created: 2015-03-04 00:13:18
+#      Desc: TODO (some description)
+#    Author: linbing, linbing@semptian.com
+#   Company: Beijing Semptian
+
+
+
+
+
+
+
+#bash auto complete
+wget http://www.caliban.org/files/redhat/RPMS/noarch/bash-completion-20060301-1.noarch.rpm 
+rpm -ivh bash-completion-20060301-1.noarch.rpm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
