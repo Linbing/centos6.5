ec4d2fa2c44d33024899403b75cc584385e14c72
linbing <840412413@qq.com>
2015-03-04 22:31:12 +0800

    add paramiko

---
+++ python/paramiko/paramiko_ssh.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#  Filename: paramiko_ssh.py
+#   Created: 2015-03-04 22:25:06
+#      Desc: TODO (some description)
+#    Author: linbing, linbing@semptian.com
+#   Company: semptian
+
+
+import paramiko 
+
+
+if __name__=="__main__":
+    print '-----'
