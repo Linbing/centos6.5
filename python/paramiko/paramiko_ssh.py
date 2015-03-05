e1600f622262375ab00732fffc6ef25ad7ed39e9
hadoop <hadoop@s2.(none)>
2015-03-04 18:58:25 -0800

    run ssh command

--- python/paramiko/paramiko_ssh.py
+++ python/paramiko/paramiko_ssh.py
@@ -7,7 +7,46 @@
 
 
 import paramiko 
+import threading
+
+def ssh_run(ip, username, passwd, cmd):
+    try:
+        ssh=paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip,22,username,passwd,timeout=5)
+#        ssh.exec_command(cmd)
+#        stdin,stdout,stderr=ssh.exec_command(cmd)
+#        stdin.write("Y")
+#        out=stdout.readline()
+#        print out
+        for m in cmd:
+            stdin,stdout,stderr=ssh.exec_command(m)
+            stdin.write("Y")
+            out=stdout.readline()
+            #for o in out:
+            print out
+        print '%s\tOK\n'%(ip)
+        ssh.close()
+    except:
+        print '%s\tError\n'%(ip)
+
+
 
 
 if __name__=="__main__":
     print '-----'
+    cmd=['touch aaa.ccc']
+    username="hadoop"
+    passwd="lb"
+    threads=[]
+    ip_a=['46','225','226','227']
+    #ip='192.168.40.46'
+    print "begin......"
+    for i in ip_a:
+        ip='192.168.40.'+str(i)
+        print ip
+        a=threading.Thread(target=ssh_run,args=(ip,username,passwd,cmd))
+        a.start()
+
+#a=threading.Thread(target=ssh_run,args=(ip,username,passwd,cmd))
+    #a.start()
