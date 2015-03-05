cffd994faf76a5899b2698e65d4217e81ac2afa1
lb <lb@localhost.(none)>
2015-03-05 02:22:59 -0800

    1.add go

---
+++ go/proj/src/cale/cale.go
@@ -0,0 +1,44 @@
+package main
+
+
+import (
+    "fmt"
+    "math"
+    "math/rand"
+)
+
+
+//func add(x int, y int) int{
+func add(x , y int) int{
+    return x+y
+}
+
+func swap(x, y string)(string,string){
+    return y,x
+
+}
+
+
+func main(){
+    
+    a,b := swap("hello","world")
+
+    fmt.Printf("----------is",rand.Intn(100))
+    fmt.Printf("\n"); 
+    fmt.Printf("now you have %g problem.",math.Nextafter(2,3))
+    fmt.Printf("\n"); 
+    
+    fmt.Printf("pi is : ",math.Pi)
+    fmt.Printf("\n"); 
+    
+    fmt.Printf("call fun add is : ",add(12,12))
+    fmt.Printf("\n"); 
+
+    fmt.Printf(a,b)
+    
+    fmt.Printf("\n"); 
+    fmt.Printf("hello,world\n")
+    fmt.Printf("hello,world\n")
+    fmt.Printf("hello,world\n")
+    fmt.Printf("hello,world\n")
+}
---
+++ go/var/main.go
@@ -0,0 +1,14 @@
+package main
+
+
+
+func main(){
+    var i int 
+    var j int8
+    i=1
+    j=int8(i+1)
+    print(i,"\t",j,"\n")
+}
+
+
+
