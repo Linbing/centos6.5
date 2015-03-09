941ec69e46a92caff688101082e0abb49c579b9e
hadoop <hadoop@s2.(none)>
2015-03-08 19:22:21 -0700

    1.add my java learning test

---
+++ java_test/src/test/base/IntegerTest.java
@@ -0,0 +1,15 @@
+package test.base;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer num1 = 297;
+        Integer num2 = 297;
+        System.out.print("num1 == num2 ? "+(num1==num2)+"\n");
+        Integer num3 = 97;
+        Integer num4 = 97;
+        System.out.println("num3 == num 4 ? "+(num3==num4));
+	}
+
+}
